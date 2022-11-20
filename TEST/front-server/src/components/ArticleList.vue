1f5510b8049b6283082cd4e1efdc89215bc71d50
hojun0318 <jaden8994@gmail.com>
2022-11-20 20:56:35 +0900

    weekend_work

---
+++ TEST/front-server/src/components/ArticleList.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="py-5">
+    <div class="container">
+      <div class="row row-cols-1 row-cols-md-6 g-4">
+        <ArticleListItem
+          v-for="article in articles"
+          :key="article.id"
+          :article="article"
+        />
+      </div>
+    </div>
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT"
+      crossorigin="anonymous"
+    />
+  </div>
+</template>
+
+<script
+  src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"
+  integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8"
+  crossorigin="anonymous"
+></script>
+
+<script>
+import ArticleListItem from '@/components/ArticleListItem'
+
+export default {
+  name: 'ArticleList',
+  components: {
+    ArticleListItem
+  },
+  computed: {
+    articles() {
+      return this.$store.state.articles
+    }
+  }
+}
+</script>
+
+<style>
+.article-list {
+  text-align: start;
+}
+</style>
---
+++ TEST/front-server/src/components/ArticleListItem.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <div class="col">
+      <div class="card h-200">
+        <img :src="this.movieURL" />
+        <div class="card-body">
+          <h5 class="text-center" style="font-weight: bold">
+            {{ article.title }}
+          </h5>
+          <div class="overview">
+            <p>{{ article.overview }}</p>
+          </div>
+          <router-link :to="{ name: 'DetailView', params: { id: article.id } }">
+            [DETAIL]
+          </router-link>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ArticleListItem',
+  props: {
+    article: Object,
+  },
+  computed: {
+    movieURL() {
+      return `https://www.themoviedb.org/t/p/w600_and_h900_bestv2/${this.article.poster_path}`
+    },
+  },
+}
+</script>
+
+<style>
+.overview {
+  height: 200px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: normal;
+  line-height: 1.2;
+  height: 6em;
+  text-align: left;
+  word-wrap: break-word;
+  display: -webkit-box;
+  -webkit-line-clamp: 5;
+  -webkit-box-orient: vertical;
+}
+</style>
